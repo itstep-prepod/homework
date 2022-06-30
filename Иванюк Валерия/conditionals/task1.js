f0fd9e5a5e0db3ef6b672c3bf5779588278c61b3
Lera <web@it.step>
2022-06-30 21:06:34 +0300

    task1

---
+++ Иванюк Валерия/conditionals/task1.js
@@ -0,0 +1,15 @@
+//1. Пользователь вводит два числа, вывести в консоль наибольшее из них.
+let number1 = +prompt('Введите число 1');
+let number2 = +prompt('Введите число 2');
+
+
+ if ( number1 > number2 ) { 
+ console.log (number1)};
+ if ( number2 > number2 ) { 
+ console.log (number2)};
+ if ( number1 = number2 ) {
+ console.log (number1)
+ }
+
+ 
+ 
