db150a44a67967d9590b724372e1772a9677429f
VladimirKunets <mr.kunec@gmail.com>
2022-06-30 20:42:09 +0300

    the first task

---
+++ Владимир Кунец/conditionals/tasks1.js
@@ -0,0 +1,8 @@
+//1. Пользователь вводит два числа, вывести в консоль наибольшее из них.
+
+let userNamberOne = prompt('Введите первое число','');
+let userNamberTwe = prompt('Введите второе число','');
+
+if (userNamberOne > userNamberTwe){
+    console.log (userNamberOne);
+} else {console.log (userNamberTwe);}
