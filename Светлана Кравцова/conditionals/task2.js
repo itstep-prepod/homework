ce754897939375d5f9a6a628aaf6cd67751a9fb3
Svetlana <swetaz@yandex.ru>
2022-07-05 14:01:13 +0300

    changed task2

--- Светлана Кравцова/conditionals/task2.js
+++ Светлана Кравцова/conditionals/task2.js
@@ -19,22 +19,31 @@ if (number1 > number2 && number1 > number3) {
 // 2. Пользователь вводит номер дня недели,
 // вывести на экран название этого дня.
 let numberWeek = +prompt('enter week number');
-if (numberWeek === 1) {
-   console.log('Monday');
-} else if (numberWeek === 2) {
-    console.log('Tuesday');
-} else if (numberWeek === 3) {
-    console.log('Wednesday');
-} else if (numberWeek === 4) {
-    console.log('Thursday');
-} else if (numberWeek === 5) {
-    console.log('Friday');
-}else if (numberWeek === 6) {
-    console.log('Saturday');
-} else if (numberWeek === 7) {
-    console.log('Sunday');
-} else {
-    console.log('you entered incorrect data, please enter the number 1-7 again');
+switch (numberWeek) {
+    case 1:
+        console.log('Monday');
+        break;
+    case 2:
+        console.log('Tuesday');
+        break; 
+    case 3:
+        console.log('Wednesday');
+        break;
+    case 4:
+        console.log('Thursday');
+        break;
+    case 5:
+        console.log('Friday');
+        break;
+    case 6:
+        console.log('Saturday');
+        break; 
+    case 7:
+        console.log('Sunday');
+        break;  
+    default:
+        console.log('you entered incorrect data, please enter the number 1-7 again');
+        break;
 }
 
 // если нужно вывести не в консоль, а на экран пользователю, то console.log заменяем на alert
