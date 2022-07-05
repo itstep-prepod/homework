7b09ce917ff184d5b88ba18f55a917e13a042cd6
Evgeni <luzov.evgeni@gmail.com>
2022-07-05 18:47:22 +0300

    fixed bugs in block switch

--- Лызов Евгений/conditionals/task2.js
+++ Лызов Евгений/conditionals/task2.js
@@ -21,26 +21,27 @@ alert(`Наибольшее из 3х чисел : ${bigNum}`);
 let numDay = +prompt('Введите номер дня недели 1-7');
 
 switch (numDay) {
-   case 1: alert('Понедельник')
-      break
-
-   case 2: alert('Вторник')
-      break
-
-   case 3: alert('Cреда')
-      break
-
-   case 4: alert('Четверг')
-      break
-
-   case 5: alert('Пятница')
-      break
-
-   case 6: alert('Суббота')
-      break
-
-   case 7: alert('Воскресенье')
-      break
+   case 1: 
+      alert('Понедельник');
+      break;
+   case 2: 
+      alert('Вторник');
+      break;
+   case 3: 
+      alert('Cреда');
+      break;
+   case 4: 
+      alert('Четверг');
+      break;
+   case 5: 
+      alert('Пятница');
+      break;
+   case 6: 
+      alert('Суббота');
+      break;
+   case 7: 
+      alert('Воскресенье');
+      break;
 }
 
 
