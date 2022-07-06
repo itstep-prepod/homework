c35fa369198852a2aca18d006eb66e2536cbe0a0
Vyacheslav <vyacheslavzhukovec@gmail.com>
2022-07-07 00:16:02 +0300

    task2_2 changed to switch2

--- Вячеслав Жуковец/conditionals/task2.js
+++ Вячеслав Жуковец/conditionals/task2.js
@@ -1,18 +1,18 @@
 // 2. Пользователь вводит 3 числа, вывести на экран наибольшее из этих 3х чисел.
 
-let user1 = +prompt('Введите первое число =');
-let user2 = +prompt('Введите второе число =');
-let user3 = +prompt('Введите третье число =');
+// let user1 = +prompt('Введите первое число =');
+// let user2 = +prompt('Введите второе число =');
+// let user3 = +prompt('Введите третье число =');
 
-if (user1>user2 && user1>user3) {
-    alert (`Наибольшее число user1 = ${user1}`);
-} else if (user2>user1 && user2>user3) {
-    alert (`Наибольшее число user2 = ${user2}`);
-} else if (user3>user1 && user3>user2) {
-    alert (`Наибольшее число user3 = ${user3}`);
-} else if (user1===user2 && user1===user3) {
-    alert ('Нет наибольшего числа');
-}
+// if (user1>user2 && user1>user3) {
+//     alert (`Наибольшее число user1 = ${user1}`);
+// } else if (user2>user1 && user2>user3) {
+//     alert (`Наибольшее число user2 = ${user2}`);
+// } else if (user3>user1 && user3>user2) {
+//     alert (`Наибольшее число user3 = ${user3}`);
+// } else if (user1===user2 && user1===user3) {
+//     alert ('Нет наибольшего числа');
+// }
 
 
 
@@ -20,20 +20,29 @@ if (user1>user2 && user1>user3) {
 
 let dayOfTheWeek = +prompt('Введите день недели =');
 
-if (dayOfTheWeek === 1){
-    alert ('Понедельник');
-} else if (dayOfTheWeek === 2) {
-    alert ('Вторник');
-} else if (dayOfTheWeek === 3) {
-    alert ('Среда');
-} else if (dayOfTheWeek === 4) {
-    alert ('Четверг');
-} else if (dayOfTheWeek === 5) {
-    alert ('Пятница');
-} else if (dayOfTheWeek === 6) {
-    alert ('Суббота');
-} else if (dayOfTheWeek === 7) {
-    alert ('Воскресенье');
-} else if (dayOfTheWeek !== (1-7)){
-    alert ('В неделе 7 дней!');
+switch (dayOfTheWeek) {
+    case 1:
+        alert ('Понедельник');
+        break;
+    case 2:
+        alert ('Вторник');
+        break;
+    case 3:
+        alert ('Среда');
+        break;
+    case 4:
+        alert ('Четверг');
+        break;
+    case 5:
+        alert ('Пятница');
+        break;
+    case 6:
+        alert ('Суббота');
+        break;
+    case 7:
+        alert ('Воскресенье');
+        break;
+    default:
+        alert('В неделе 7 дней!');
+        break;
 }
