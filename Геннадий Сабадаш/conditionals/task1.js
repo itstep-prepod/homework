d20ecf7fc580245aab8b67aa6876cf0a91d6bc31
Sabadash Gena <gendolh714@gmail.com>
2022-07-10 21:27:36 +0300

    Tasks

--- Геннадий Сабадаш/conditionals/task1.js
+++ Геннадий Сабадаш/conditionals/task1.js
@@ -1,12 +1,12 @@
 //1. Пользователь вводит два числа, вывести в консоль наибольшее из них.
 
-let userNamber1 = +prompt('введите число 1');
-let userNamber2 = +prompt('введите число 2');
+// let userNamber1 = +prompt('введите число 1');
+// let userNamber2 = +prompt('введите число 2');
 
-if (userNamber1 > userNamber2 ){
-    console.log(userNamber1);
-} else if (userNamber2 > userNamber1){
-    console.log(userNamber2);    
-} else if (userNamber1 === userNamber2){
-    console.log ("числа равны"); 
-}
+// if (userNamber1 > userNamber2 ){
+//     console.log(userNamber1);
+// } else if (userNamber2 > userNamber1){
+//     console.log(userNamber2);    
+// } else if (userNamber1 === userNamber2){
+//     console.log ("числа равны"); 
+// }
