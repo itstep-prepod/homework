c5d605ae1a802c66dd5b0ef722e17650f14cecb0
Leva <parimalleva@gmail.com>
2022-07-13 09:20:51 +0300

    uPPPGrade task5

--- Олег Леванов/conditionals/task5.js
+++ Олег Леванов/conditionals/task5.js
@@ -1,26 +1,43 @@
-//  Создать программу в которой сравнивается возраст 
-// двух человек (Федота и Митрофана).
-//  Если возраст Федота больше чем возраст Митрофана, 
-// то должно выводится сообщение, в котором указана 
-// разница в возрасте: «Федот старше Митрофана на 3 ».
-//  Если возраст Митрофана больше, то выводится такое 
-// сообщение: «Митрофан старше Федота на 5».
-//  Если возраст обоих человек один и тот же, то 
-// выводится такое сообщение: «Федот и Митрофан 
-// одного возраста».
+// Создать программу в которой сравнивается возраст 
+//  двух человек (Федота и Митрофана).
+// Если возраст Федота больше чем возраст Митрофана, 
+//  то должно выводится сообщение, в котором указана 
+//  разница в возрасте: «Федот старше Митрофана на 3 ».
+// Если возраст Митрофана больше, то выводится такое 
+//  сообщение: «Митрофан старше Федота на 5».
+// Если возраст обоих человек один и тот же, то 
+//  выводится такое сообщение: «Федот и Митрофан 
+//  одного возраста».
 
-let ageFedot = +prompt('Введите возраст Федота');
-let ageMitrofan = +prompt('Введите возраст Митрофана');
-let differentAge;
-if (ageFedot > ageMitrofan) {
-    differentAge = ageFedot - ageMitrofan;
-    alert(`Федот старше Митрофана на ${differentAge}`);
-} else if (ageMitrofan > ageFedot) {
-    differentAge = ageMitrofan - ageFedot;
-    alert(`Митрофан старше Федота на ${differentAge}`);
-} else if (ageFedot === ageMitrofan) {
-    alert('Федот и Митрофан одного возраста');
-} else if (isNaN(ageFedot) || isNaN(ageMitrofan)){
-    alert('Введите корректные данные');
-};
+let inputAgeFedot = prompt('Введите возраст Федота');
+let inputAgeMitrofan = prompt('Введите возраст Митрофана');
+let ageFedot;
+let ageMitrofan;
+if (inputAgeFedot === null || inputAgeFedot === ''
+    || inputAgeMitrofan === null || inputAgeMitrofan === '') {
+    alert('Ошибка ввода есть пустая строка!');
+} else {
+
+    ageFedot = Number(inputAgeFedot);
+    ageMitrofan = Number(inputAgeMitrofan);
+
+    if (isNaN(ageFedot) || isNaN(ageMitrofan)) {
+        alert('Ошибка надо два числа!');
+    } else {
+
+        if (ageFedot > 120 || ageMitrofan > 120) {
+            alert('Кто то врет! Слишком большой возраст.');
+        } else {
+            if (ageFedot > ageMitrofan) {
+                differentAge = ageFedot - ageMitrofan;
+                alert(`Федот старше Митрофана на ${differentAge}`);
+            } else if (ageMitrofan > ageFedot) {
+                differentAge = ageMitrofan - ageFedot;
+                alert(`Митрофан старше Федота на ${differentAge}`);
+            } else {
+                alert('Федот и Митрофан одного возраста');
+            }
+        }
+    }
+}
 
