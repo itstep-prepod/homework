73a1b556f2dcd1167e58d98f8208883571154004
Leva <parimalleva@gmail.com>
2022-07-13 14:11:41 +0300

    add task1(3.)

--- Олег Леванов/loop/task1.js
+++ Олег Леванов/loop/task1.js
@@ -39,12 +39,53 @@
 // 2. написать прогамму, которая вычисляет сумму чисел от 1 до n
 // (n всегда положительное) + проверить число n на число
 
+// let inputNumber;
+// let isWork = false;
+// let number = 0;
+// do {
+
+//     inputNumber = prompt("Введите число.\n(число всегда положительное)");
+
+//     if (inputNumber === null || inputNumber === '') {
+//         console.log('Надо было вести число а не пустую строку.');
+//     } else {
+
+//         number = Number(inputNumber);
+//         if (isNaN(number)) {
+//             console.log('Введено не число!\nНадо число.');
+//         } else if (!Number.isSafeInteger(number)) {
+//             console.log('Переданное значение НЕ является безопасным целым числом.');
+//         } else if (number < 0) {
+//             console.log('Число должно быть положительное!');
+//         } else if (number === 0) {
+//             console.log('Число 0 не является ни положительным, ни отрицательным числом.');
+//         } else {
+//             isWork = true;
+//         }
+//     }
+
+// } while (isWork === false);
+
+// let sum = 0;
+// for (let i = 1; i <= number; i++) {
+//     sum += i;
+// }
+// console.log(`Сумма числа ${number} составила ${sum}`); //n = 100 -> sum = 5050
+
+// 3. написать программу воторая определяет количество
+// положительных, отрицательных и нулей
+// в начале программы пользователь воодит количество чисел которое намерен вводить
+// пример вывода в консоли
+// положительных: 2
+// отрицательных: 4
+// нулей: 1
+
 let inputNumber;
 let isWork = false;
 let number = 0;
 do {
 
-    inputNumber = prompt("Введите число.\n(число всегда положительное)");
+    inputNumber = prompt("Введите количество чисел котрые будете вводить.");
 
     if (inputNumber === null || inputNumber === '') {
         console.log('Надо было вести число а не пустую строку.');
@@ -58,7 +99,7 @@ do {
         } else if (number < 0) {
             console.log('Число должно быть положительное!');
         } else if (number === 0) {
-            console.log('Число 0 не является ни положительным, ни отрицательным числом.');
+            console.log('Ну алее хотяб одно число.\nКоличестВООО');
         } else {
             isWork = true;
         }
@@ -66,19 +107,47 @@ do {
 
 } while (isWork === false);
 
-let sum = 0;
-for (let i = 1; i <= number; i++) {
-    sum += i;
-}
-console.log(`Сумма числа ${number} составила ${sum}`); //n = 100 -> sum = 5050
+let countZerro = 0;
+let countNegative = 0;
+let countPositive = 0;
+let inputNumberFind;
+let isStep = false;
+let numberFind = 0;
+for (let i = 1; i < number + 1; i++) {
+    do {
 
-// 3. написать программу воторая определяет количество
-// положительных, отрицательных и нулей
-// в начале программы пользователь воодит количество чисел которое намерен вводить
-// пример вывода в консоли
-// положительных: 2
-// отрицательных: 4
-// нулей: 1
+        inputNumberFind = prompt(`Введите ${i} число для проверки.`);
+
+        if (inputNumberFind === null || inputNumberFind === '') {
+            console.log('Надо было вести число а не пустую строку.');
+        } else {
+
+            numberFind = Number(inputNumberFind);
+            if (isNaN(numberFind)) {
+                console.log('Введено не число!\nНадо число.');
+            } else if (!Number.isSafeInteger(numberFind)) {
+                console.log('Переданное значение НЕ является безопасным целым числом.');
+            } else {
+                isStep = true;
+            }
+        }
+
+    } while (isStep === false);
+
+    if (numberFind > 0) {
+        countPositive++;
+    } else if (numberFind < 0) {
+        countNegative++;
+    } else {
+        countZerro++;
+    }
+
+}
+
+console.log('===============================');
+console.log('положительных: ' + countPositive);
+console.log('отрицательных: ' + countNegative);
+console.log('нулей: ' + countZerro);
 
 // 4. написать программу которая выводит среднее арифметическое
 // n чисел введенных пользователем. Ввод чисел прекращается когда
