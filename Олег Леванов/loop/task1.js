c8b06a9347fd21124270f445a3f789c6b5616e3e
Leva <parimalleva@gmail.com>
2022-07-13 13:23:05 +0300

    add task1(2.)

--- Олег Леванов/loop/task1.js
+++ Олег Леванов/loop/task1.js
@@ -1,12 +1,50 @@
 // 1. пользователь вводит число n 
 // написать цикл который выводит все числа от n до 0
 // (n может быть отрицательным) 
+// let inputNumber;
+// let isWork = false;
+// let number = 0;
+// do {
+
+//     inputNumber = prompt("Введите число.\n(число может быть отрицательным)");
+
+//     if (inputNumber === null || inputNumber === '') {
+//         console.log('Надо было вести число а не пустую строку.');
+//     } else {
+
+//         number = Number(inputNumber);
+//         if (isNaN(number)) {
+//             console.log('Введено не число!\nНадо число.');
+//         } else if (!Number.isSafeInteger(number)) {
+//             console.log('Переданное значение НЕ является безопасным целым числом.');
+//         } else {
+//             isWork = true;
+//         }
+//     }
+
+// } while (isWork === false);
+
+// if (number > 0) {
+//     for (let i = number; i > 0; i--) {
+//         console.log(i);
+//     }
+// } else if (number < 0) {
+//     for (let i = number; i < 0; i++) {
+//         console.log(i);
+//     }
+// } else {
+//     console.log('До нуля чисел нет.');
+// }
+
+// 2. написать прогамму, которая вычисляет сумму чисел от 1 до n
+// (n всегда положительное) + проверить число n на число
+
 let inputNumber;
 let isWork = false;
 let number = 0;
 do {
 
-    inputNumber = prompt("Введите число.\n(число может быть отрицательным)");
+    inputNumber = prompt("Введите число.\n(число всегда положительное)");
 
     if (inputNumber === null || inputNumber === '') {
         console.log('Надо было вести число а не пустую строку.');
@@ -17,6 +55,10 @@ do {
             console.log('Введено не число!\nНадо число.');
         } else if (!Number.isSafeInteger(number)) {
             console.log('Переданное значение НЕ является безопасным целым числом.');
+        } else if (number < 0) {
+            console.log('Число должно быть положительное!');
+        } else if (number === 0) {
+            console.log('Число 0 не является ни положительным, ни отрицательным числом.');
         } else {
             isWork = true;
         }
@@ -24,20 +66,11 @@ do {
 
 } while (isWork === false);
 
-if (number > 0) {
-    for (let i = number; i > 0; i--) {
-        console.log(i);
-    }
-} else if (number < 0) {
-    for (let i = number; i < 0; i++) {
-        console.log(i);
-    }
-} else {
-    console.log('До нуля чисел нет.');
+let sum = 0;
+for (let i = 1; i <= number; i++) {
+    sum += i;
 }
-
-// 2. написать прогамму, которая вычисляет сумму чисел от 1 до n
-// (n всегда положительное) + проверить число n на число
+console.log(`Сумма числа ${number} составила ${sum}`); //n = 100 -> sum = 5050
 
 // 3. написать программу воторая определяет количество
 // положительных, отрицательных и нулей
