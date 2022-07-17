8b074c7f32bc5cc61bd5ed56734f58ed4a39cf43
Bogdan <zuyrinbogdank@gmail.com>
2022-07-17 11:37:21 +0300

    task 1.3 loops

--- Зюрин Богдан/loops/index.js
+++ Зюрин Богдан/loops/index.js
@@ -1,15 +1,43 @@
 // 2. написать прогамму, которая вычисляет сумму чисел от 1 до n 
 // (n всегда положительное) + проверить число n на число
 
-let userNumber = +prompt ('Введи положительное число');
-let sum = 0;
-if (userNumber <= 0) {
-    userNumber = +prompt ('Введи положительное число');
-} else if (isNaN (userNumber) === true) {
-    userNumber = +prompt ('Введи положительное число');
-} else {
-    for (let i = 0; i <= userNumber; i++) {
-        sum += i;
+// let userNumber = +prompt ('Введи положительное число');
+// let sum = 0;
+// if (userNumber <= 0) {
+//     userNumber = +prompt ('Введи положительное число');
+// } else if (isNaN (userNumber) === true) {
+//     userNumber = +prompt ('Введи положительное число');
+// } else {
+//     for (let i = 0; i <= userNumber; i++) {
+//         sum += i;
+//     }
+//     console.log (sum);
+// }
+
+// 3. написать программу воторая определяет количество 
+// положительных, отрицательных и нулей
+// в начале программы пользователь воодит количество чисел которое намерен вводить
+// пример вывода в консоли
+// положительных: 2
+// отрицательных: 4
+// нулей: 1
+
+let userAmountOfNumbers = +prompt ('Скольско чисел вы хотите ввести?');
+let userNumber = 0;
+let positive = 0;
+let negative = 0;
+let zero = 0;
+for (let i = 0; i < userAmountOfNumbers; i++) {
+
+    userNumber = +prompt ('введите число');
+
+    if (userNumber > 0) {
+        positive += 1;
+    } else if (userNumber < 0) {
+        negative += 1;
+    } else {
+        zero += 1;
     }
-    console.log (sum);
+    
 }
+console.log (`положительных: ${positive}, отрицательных: ${negative}, нулей: ${zero}`);
