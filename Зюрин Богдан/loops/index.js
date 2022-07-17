dbd8a7ccf633af22a7a801b07059a010283c7c4d
Bogdan <zuyrinbogdank@gmail.com>
2022-07-17 12:12:41 +0300

    task 1.4 loops

--- Зюрин Богдан/loops/index.js
+++ Зюрин Богдан/loops/index.js
@@ -22,22 +22,43 @@
 // отрицательных: 4
 // нулей: 1
 
-let userAmountOfNumbers = +prompt ('Скольско чисел вы хотите ввести?');
-let userNumber = 0;
-let positive = 0;
-let negative = 0;
-let zero = 0;
-for (let i = 0; i < userAmountOfNumbers; i++) {
+// let userAmountOfNumbers = +prompt ('Скольско чисел вы хотите ввести?');
+// let userNumber = 0;
+// let positive = 0;
+// let negative = 0;
+// let zero = 0;
+// for (let i = 0; i < userAmountOfNumbers; i++) {
 
-    userNumber = +prompt ('введите число');
+//     userNumber = +prompt ('введите число');
 
-    if (userNumber > 0) {
-        positive += 1;
-    } else if (userNumber < 0) {
-        negative += 1;
-    } else {
-        zero += 1;
-    }
+//     if (userNumber > 0) {
+//         positive += 1;
+//     } else if (userNumber < 0) {
+//         negative += 1;
+//     } else {
+//         zero += 1;
+//     }
     
+// }
+// console.log (`положительных: ${positive}, отрицательных: ${negative}, нулей: ${zero}`);
+
+// 4. написать программу которая выводит среднее арифметическое 
+// n чисел введенных пользователем. Ввод чисел прекращается когда
+// нажали "отмена", после чего выводится среднее арифметическое
+
+let userNumber = 0;
+let sum = 0;
+
+for (let i = 0; ; i++) {
+
+    userNumber = prompt (`Введи число`);
+
+    sum += +userNumber;
+
+    if (userNumber ===  null || userNumber === ``) {
+        console.log (sum / i);
+        break;
+    } 
 }
-console.log (`положительных: ${positive}, отрицательных: ${negative}, нулей: ${zero}`);
+
+
