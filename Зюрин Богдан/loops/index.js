cdfde17854398bf643d7e86058279a7c64d950dd
Bogdan <zuyrinbogdank@gmail.com>
2022-07-19 17:45:43 +0300

    task 1.6 loops fixed

--- Зюрин Богдан/loops/index.js
+++ Зюрин Богдан/loops/index.js
@@ -88,7 +88,7 @@ for (humsters; humsters > 0; ) {
     let attempt = prompt ('достань хомяков');
     humsters -= attempt;
     counter += 1;
-    if (attempt > 100) {
+    if (+attempt > 100) {
         alert ('столько нет');
         humsters = 100;
     } else if (humsters === 0) {
